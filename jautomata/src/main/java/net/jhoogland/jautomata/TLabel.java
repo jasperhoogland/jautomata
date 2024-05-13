a506a9edb912baa96227c695a369787abfb6b756
Robert Müller <robytemueller@gmail.com>
2024-05-13 13:16:59 +0200

    Refactor equals and hashCode methods of TLabel
    Fix unchecked cast warning. Use Objects.equals and hash functions.

--- jautomata/src/main/java/net/jhoogland/jautomata/TLabel.java
+++ jautomata/src/main/java/net/jhoogland/jautomata/TLabel.java
@@ -1,5 +1,7 @@
 package net.jhoogland.jautomata;
 
+import java.util.Objects;
+
 /**
  * Implements an input/output label pair for transducers.
  *  
@@ -54,29 +56,23 @@ public class TLabel<I, O>
 	}
 	
 	@Override
-	public boolean equals(Object obj) 
-	{		
-		if (obj instanceof TLabel)
-		{
-			TLabel<I, O> other = (TLabel<I, O>) obj;
-			return equals(this.iLabel, other.iLabel) && equals(this.oLabel, other.oLabel);
-		}
-		else return false;
-	}
-	
-	private boolean equals(Object l1, Object l2)
+	public boolean equals(Object obj)
 	{
-		return l1 == null ? l2 == null : l1.equals(l2);
-	}
-	
-	@Override
-	public int hashCode() 
-	{		
-		return hashCode(iLabel) + hashCode(oLabel);
+		if (obj == this)
+		{
+			return true;
+		}
+		if (!(obj instanceof TLabel))
+		{
+			return false;
+		}
+		TLabel<?, ?> other = (TLabel<?, ?>) obj;
+		return Objects.equals(this.iLabel, other.iLabel) && Objects.equals(this.oLabel, other.oLabel);
 	}
 
-	private int hashCode(Object l) 
-	{		
-		return l == null ? 0 : l.hashCode();
+	@Override
+	public int hashCode()
+	{
+		return Objects.hash(iLabel, oLabel);
 	}
 }
